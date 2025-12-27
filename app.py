a566807dda735ccc4bff3b4835d30fab5d0e1e51
Aryan1337x <aryanpratap012008@gmail.com>
2025-12-27 18:11:58 +0530

    #1

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import os
+
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+
 import time
 from flask import Flask, render_template, request, url_for, send_from_directory
 from utils import save_uploaded_file, ALLOWED_EXTENSIONS
